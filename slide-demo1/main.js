fcabda5391348ce60c59a40a09e222b7d4fe4b2c
dongxx <dongxiaoxing0@gmail.com>
2019-07-20 02:58:47 +0800

    任务30 轮播修改

--- slide-demo1/main.js
+++ slide-demo1/main.js
@@ -3,12 +3,12 @@ var $buttons = $('.buttons')
 for (let i = 0; i < $buttons.length; i++) {
     $($buttons[i]).on('click', function (x) {
         let index = $(x.currentTarget).index()
-        let p = -(index - 1) * 400
+        console.log(index)
+        let p = -index * 400
         $('#images').css({
             transform: 'translateX(' + p + 'px)'
         })
-        $buttons.eq(index - 1).addClass('red').siblings().removeClass('red')
-        n = index - 1
+        $buttons.eq(index).addClass('red').siblings().removeClass('red')
     })
 }
 
@@ -23,7 +23,7 @@ $('.window').on('mouseenter', function () {
     window.clearInterval(timerId)
 })
 $('.window').on('mouseleave', function () {
-    var timerId = setInterval(function () {
+    timerId = setInterval(function () {
         $($buttons[n % size]).trigger('click')
         n += 1
     }, 2000)
