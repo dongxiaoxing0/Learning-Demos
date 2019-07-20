72a46f35c9c817ec374c77ee6f54e3a812600493
dongxx <dongxiaoxing0@gmail.com>
2019-07-20 21:30:47 +0800

    任务31 实现无缝轮播

---
+++ slide-demo2/main.js
@@ -0,0 +1,26 @@
+var $buttons = $('#buttons > span')
+for (let i = 0; i < $buttons.length; i++) {
+    $($buttons[i]).on('click', function (x) {
+        let index = $(x.currentTarget).index()
+        let p = $('.show').index()
+        var $images = $('#images > img')
+        if (p !== index) {
+            $images.eq(p).addClass('leave').removeClass('show enter')
+            $images.eq(p).one('transitionend', function (x) {
+                $images.eq(p).addClass('enter').removeClass('leave show')
+            })
+        }
+        $images.eq(index).addClass('show').removeClass('leave enter')
+        $buttons.eq(index).addClass('active').siblings().removeClass('active')
+
+    })
+}
+
+var n = 0
+var size = $buttons.length
+var timerId = setInterval(function(){
+    $buttons.eq(n % size).trigger('click')
+    console.log()
+    n += 1
+    console.log(n % size)
+},2000)
